ad3a317b87e3036508ba9b07032526ba0b17c91e
JSG1987 <JGentry0058@gmail.com>
2019-08-27 14:21:10 -0500

    HW6

---
+++ pg82 problem 6/Source.cpp
@@ -0,0 +1,20 @@
+// the goal of this program is to calculate annual pay
+#include<iostream>
+#include<iomanip>
+
+
+using namespace std;
+
+int main()
+{
+	float payAmount = 2200.0;
+	float payPeriods = 26;
+	float annualPay = payAmount * payPeriods;
+	cout << "The annual pay is " << annualPay << endl;
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
